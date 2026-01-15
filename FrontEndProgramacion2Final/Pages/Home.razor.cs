c7c0cc60d6c2fe6b12c6926cdf616eb8426cf516
Lautaro Tamborini <lautarotamborini@gmail.com>
2026-01-15 13:13:30 -0300

    Agregar archivos de proyecto.

---
+++ FrontEndProgramacion2Final/Pages/Home.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace FrontEndProgramacion2Final.Pages
+{
+    public partial class Home : ComponentBase
+    {
+        protected string Title { get; set; } = "Home";
+
+        protected int CurrentCount { get; set; }
+
+        protected void IncrementCount()
+        {
+            CurrentCount++;
+        }
+    }
+}
